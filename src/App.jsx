e94fc89f39347bbe8f57efa8ca3ecd4bd7b32e20
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-03-03 07:10:39 +0330

    create usefetch hook and locationlist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { ToastBar, Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./component/Header/Header";
+import LocationList from "./component/LoctionList/locationList";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Header />
+      <LocationList />
     </div>
   );
 }
