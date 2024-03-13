70b27fcf2a8edce5b0c6761abead7d77274d55ec
elhamgholami1995 <elhamgholami5531@gmail.com>
2024-03-13 06:11:38 +0330

    fetch location data in addnewbookmark.jsx

--- src/component/AddNewBookmark/AddNewBookmark.jsx
+++ src/component/AddNewBookmark/AddNewBookmark.jsx
@@ -1,21 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useUrlLocation from "../hooks/useUrlLocation";
+import axios from "axios";
+import Loader from "../Loader/Loader";
+import ReactCountryFlag from "react-country-flag";
 
+// function getFlagEmoji(countryCode) {
+//   const codePoints = countryCode
+//     .toUpperCase()
+//     .split("")
+//     .map((char) => 127397 + char.charCodeAt());
+//   return String.fromCodePoint(...codePoints);
+// }
+
+const BASE_GEOCODING_URL =
+  "https://api.bigdatacloud.net/data/reverse-geocode-client";
 function AddNewBookmark() {
   const [lat, lng] = useUrlLocation();
   const navigate = useNavigate();
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [isLoadingGeoCoding, setIsLoadingGeoCoding] = useState(false);
+  const [geoCodingError, setGeoCodingError] = useState(null);
+  useEffect(() => {
+    if (!lat || !lng) return;
+    async function fetchLocationData() {
+      setIsLoadingGeoCoding(true);
+      setGeoCodingError(null);
+      try {
+        const { data } = await axios.get(
+          `${BASE_GEOCODING_URL}?latitude=${lat}&longitude=${lng}`
+        );
+        if (!data.countryCode)
+          throw new Error(
+            "this Location is not a city,! please click somewhere else..."
+          );
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+        setCountryCode(data.countryCode);
+      } catch (error) {
+        setGeoCodingError(error.message);
+      } finally {
+        setIsLoadingGeoCoding(false);
+      }
+    }
+    fetchLocationData();
+  }, [lat, lng]);
+
+  if (isLoadingGeoCoding) return <Loader />;
+  if (geoCodingError) return <p>{geoCodingError}</p>;
   return (
     <div>
       <h2 className="singleBookmarkTitle">Bookmark New Location</h2>
       <form className="form">
         <div className="formControl">
           <label htmlFor="cityName">cityName</label>
-          <input type="text" name="cityName" id="cityName" />
+          <input
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            type="text"
+            name="cityName"
+            id="cityName"
+          />
         </div>
         <div className="formControl">
           <label htmlFor="country">country</label>
-          <input type="text" name="country" id="country" />
+          <input
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            type="text"
+            name="country"
+            id="country"
+          />
+          <ReactCountryFlag className="flag" svg countryCode={countryCode} />
+          {/* <span className="flag">{countryCode}</span> */}
         </div>
         <div className="buttons">
           <button
